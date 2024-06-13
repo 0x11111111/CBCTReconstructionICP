9c5ea11127e5273da76ab448ce7e7114bc8d7209
0x11111111 <dypro_mike@outlook.com>
2024-06-13 11:02:02 +0800

    Finished recon from sliced DCMs. Set selective bounding box. Next is to change to recon from TIFF.

--- src/main program/main.cpp
+++ src/main program/main.cpp
@@ -10,6 +10,13 @@
 #include <MRMesh/MRMeshTopology.h>
 #include <MRMesh/MRExpected.h>
 
+#include <MRMesh/MRVoxelsLoad.h>
+#include <MRMesh/MRObjectVoxels.h>
+#include <MRMesh/MRBox.h>
+#include <MRMesh/MRAffineXf.h>
+
+#include <filesystem>
+
 vtkRenderPipeline* pipeline;
 
 /**
@@ -60,41 +67,68 @@ void LeftRelease(vtkObject* caller, long unsigned int eventId, void* clientData,
 {
 }
 	
+namespace fs = std::filesystem;
 
 int main()
 {
 	using namespace MR;
 	pipeline = new vtkRenderPipeline();
 
-	MR::Mesh mrmesh = *MR::MeshLoad::fromAnySupportedFormat("data/11.stl");
-	std::vector<vertex_descriptor> vertices_list(mrmesh.topology.vertSize());
-	
-	SurfaceMesh sm = MRMeshToSurfaceMesh(mrmesh);
-	Mesh converted = SurfaceMeshToMRMesh(sm);
-	
-	MeshSave::toAnySupportedFormat(converted, "data/converted.stl");
+	fs::path base_folder_path = "data/02";
+	fs::path cbct_folder_path = base_folder_path / "ct.nii";
+	fs::path intraoral_folder_path = base_folder_path / "口扫";
 
-	RenderPolydata(MRMeshToPolyData(mrmesh), pipeline->Renderer);
+	fs::path cbct_file_path = cbct_folder_path / "ct.nii";
+	fs::path dicom_file_path = base_folder_path / "dicom";
+	fs::path upper_jaw_file_path = intraoral_folder_path / "UpperJaw.stl";
+	fs::path lower_jaw_file_path = intraoral_folder_path / "LowerJaw.stl";
 
-	vtkNew<vtkSTLReader> reader;
-	reader->SetFileName("data/converted.stl");
-	reader->Update();
-	vtkSmartPointer<vtkPolyData> polydata = reader->GetOutput();
 
-	Mesh converted_from_pd = PolyDataToMRMesh(polydata);
-	MeshSave::toAnySupportedFormat(converted_from_pd, "data/converted_from_pd.ply");
+	auto voxel_load = VoxelsLoad::loadDCMFolder(dicom_file_path);
+	//std::cout << voxel_load.error() << std::endl;
+	auto vdb = voxel_load.value().vdbVolume;
+	std::cout << vdb.voxelSize[0] << vdb.voxelSize[1] << vdb.voxelSize[2] << std::endl;
+
+	auto dims = vdb.dims;
+
+	auto dim_x = dims.x;
+	auto dim_y = dims.y;
+	auto dim_z = dims.z;
+
+	auto max_x = dim_x / 2;
+	auto max_y = dim_y;
+	auto max_z = dim_z / 16 * 5;
+	auto min_z = dim_z / 16 * 3;
+
+	auto active_bounds = Box3i(Vector3i(0, 0, min_z), Vector3i(max_x, max_y, max_z));
+
+	ObjectVoxels obj_voxels;
+	obj_voxels.construct(vdb);
+	obj_voxels.applyScale(1.0f / vdb.voxelSize[0] / 3.5f);
+	obj_voxels.setActiveBounds(active_bounds);
+	obj_voxels.setDualMarchingCubes(true);
+	obj_voxels.setIsoValue(.444f);
+	Mesh cbct_recon_mr(*obj_voxels.mesh());
+
+	auto rot_matrix = Matrix3f::rotation(Vector3f{ 0, 0, 1. }, PI / 2);
+	auto rot_xf = AffineXf3f::linear(rot_matrix);
+
+	cbct_recon_mr.transform(rot_xf);
+	MeshSave::toAnySupportedFormat(cbct_recon_mr, cbct_folder_path / "cbct_recon_mr.ply");
+
+
 
 	// Set up the camera and interactor.
-	pipeline->Renderer->GetActiveCamera()->SetParallelProjection(1);
-	pipeline->Renderer->ResetCamera();
-	// Set up the callback functions for mouse events.
-	pipeline->addObserver(vtkCommand::LeftButtonPressEvent, LeftPress);
-	pipeline->addObserver(vtkCommand::MouseMoveEvent, MouseMove);
-	pipeline->addObserver(vtkCommand::LeftButtonReleaseEvent, LeftRelease);
-	pipeline->addObserver(vtkCommand::RightButtonPressEvent, RightPress);
-	pipeline->addObserver(vtkCommand::RightButtonReleaseEvent, RightRelease);
+	//pipeline->Renderer->GetActiveCamera()->SetParallelProjection(1);
+	//pipeline->Renderer->ResetCamera();
+	//// Set up the callback functions for mouse events.
+	//pipeline->addObserver(vtkCommand::LeftButtonPressEvent, LeftPress);
+	//pipeline->addObserver(vtkCommand::MouseMoveEvent, MouseMove);
+	//pipeline->addObserver(vtkCommand::LeftButtonReleaseEvent, LeftRelease);
+	//pipeline->addObserver(vtkCommand::RightButtonPressEvent, RightPress);
+	//pipeline->addObserver(vtkCommand::RightButtonReleaseEvent, RightRelease);
 
-	pipeline->RenderWindowInteractor->Start();
+	//pipeline->RenderWindowInteractor->Start();
 
 	// Clean up the pipeline after each run.
 	delete pipeline;
